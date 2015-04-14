bc6e0df74e82225aa7253fba96d815e3769c3db7
Basti <bastian.morath@gmx.ch>
2015-04-14 10:25:00 -0700

    Initial Commit

---
+++ Bastian MorathTests/Bastian_MorathTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bastian_MorathTests.swift
+//  Bastian MorathTests
+//
+//  Created by Bastian Morath on 14/04/15.
+//  Copyright (c) 2015 Bastian Morath. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Bastian_MorathTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
